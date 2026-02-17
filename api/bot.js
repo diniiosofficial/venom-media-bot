ef15cf2bece817ba3b651460308106d3c24ea259
DINI-IOS <diniiosofficial@gmail.com>
2026-02-18 02:10:23 +0530

    Update target channels and improve message handling

--- api/bot.js
+++ api/bot.js
@@ -16,7 +16,7 @@ const ADMIN_ID = 5707956654;
 const TARGET_CHANNELS = [
   -1002762374328, // VENOM LOADER
   -1002683334976, // VENOM FEEDBACK
-  -1002558925715, // VENOM FREE MODZ
+  -1003537781106, // VENOM FREE MODZ
   -1001858673142, // LEO&KNIGHT CHEAT
   -1003203628581  // 𝐕𝐄𝐍𝐎𝐌 𝐒𝐄𝐑𝐕𝐄𝐑™
 ];
@@ -192,6 +192,7 @@ module.exports = async (req, res) => {
     res.status(500).send("Internal Error");
   }
 };
+
 
 
 
